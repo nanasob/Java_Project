9f33370d6d077c2dea9fbfb4cae717be945aa7a2
nanasobansode <nanasobansode11@gmail.com>
2025-04-18 13:49:49 +0530

    adding new project

---
+++ Addition.java
@@ -0,0 +1,13 @@
+class Addition{
+    public static void main(String args[]){
+        Addition a = new Addition();
+        int sum = a.add(5, 6);
+        System.out.println("Addition is :: "+sum);
+
+    }
+
+    int add(int a, int b){
+        int c = a + b;
+        return c;
+    }
+}
